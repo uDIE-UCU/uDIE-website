69c838af18bce88ccbf9f02a3543ed64c407dcaf
beluume <maria.mendes@correo.ucu.edu.uy>
2025-09-02 15:40:30 -0300

    mejoras en el php

--- send-mail.php
+++ send-mail.php
@@ -1,7 +1,9 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name    = htmlspecialchars($_POST['name']);
+    $surname = htmlspecialchars($_POST['surname']);
     $subject = htmlspecialchars($_POST['subject']);
+    $email   = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
     $to      = "microdieucu@gmail.com";
@@ -9,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $headers .= "Reply-To: noreply@microdie.ucu.edu.uy\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-    $fullMessage = "Name: $name\nSubject: $subject\n\nMessage:\n$message";
+    $fullMessage = "Name: $name $surname\nEmail: $email\nSubject: $subject\n\nMessage:\n$message";
 
     if (mail($to, $subject, $fullMessage, $headers)) {
         echo "Message sent successfully.";
