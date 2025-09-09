5263335df2dc9b1979828cfa69b11b8d2869508f
beluume <maria.mendes@correo.ucu.edu.uy>
2025-09-09 10:57:56 -0300

    cambios en send mail

--- send-mail.php
+++ send-mail.php
@@ -2,13 +2,13 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name    = htmlspecialchars($_POST['name']);
     $surname = htmlspecialchars($_POST['surname']);
-    $subject = htmlspecialchars($_POST['subject']);
     $email   = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
     $message = htmlspecialchars($_POST['message']);
 
     $to      = "microdieucu@gmail.com";
     $headers = "From: noreply@microdie.ucu.edu.uy\r\n";
-    $headers .= "Reply-To: noreply@microdie.ucu.edu.uy\r\n";
+    $headers .= "Reply-To: $email\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
     $fullMessage = "Name: $name $surname\nEmail: $email\nSubject: $subject\n\nMessage:\n$message";
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (mail($to, $subject, $fullMessage, $headers)) {
         echo "Message sent successfully.";
     } else {
-        echo "There was an error sending the message.";
+        echo "Error sending message.";
     }
 }
 ?>
